95109253f448d1b0c4b7d9f94524a3352cd33173
IsacHolstad <wisac22@gmail.com>
2022-03-28 16:08:49 +0200

    made searchbox ready to make filter by search

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const myApi = 'https://api-mobilespecs.azharimm.site/v2/brands';
+const myApi = "https://api-mobilespecs.azharimm.site/v2/brands";
 console.log(myApi);
 
 const containerWithPhones = document.querySelector(".container");
-console.log(containerWithPhones);
+
+
+async function phoneNames() {
+    try{
+        const response = await fetch(myApi);
+        console.log(response);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        const phoneData = responseJSON.data;
+        for (let i = 0; i < phoneData.length; i++) {
+            console.log(phoneData[i].brand_name)
+            if (i === 15) {
+                break
+            }
+            containerWithPhones.innerHTML += `<li class="phonenamesli">${phoneData[i].brand_name}</li>`
+        }
+
+    }
+    catch(error) {
+        containerWithPhones.innerHTML += `<li>there is an error happening</li>`
+    }
+}
+phoneNames()
+
+
+//add filter by search to searchbox in navigation top header
