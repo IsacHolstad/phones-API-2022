f8af78feb54b1038e1d6df64a96c296a9a1c7565
IsacHolstad <wisac22@gmail.com>
2022-03-28 14:42:05 +0200

    made a simple navigation

---
+++ script.js
@@ -0,0 +1,5 @@
+const myApi = 'https://api-mobilespecs.azharimm.site/v2/brands';
+console.log(myApi);
+
+const containerWithPhones = document.querySelector(".container");
+console.log(containerWithPhones);
