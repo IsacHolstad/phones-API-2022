36c815044b562fd88926a08fc1197dc86d6746b9
IsacHolstad <wisac22@gmail.com>
2022-03-29 16:26:01 +0200

    catching dat on details page

--- phonedetails.js
+++ phonedetails.js
@@ -1,3 +1,26 @@
-const detailApi = "https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2";
+const detailApi = "https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?";
 console.log(detailApi);
 
+const phoneContainer = document.querySelector(".container");
+
+console.log(phoneContainer);
+
+async function detailContainer() {
+    try {
+        const response = await fetch(detailApi);
+        console.log(response);
+        const responseJSON = await response.json(); // convert the response to json data
+        console.log(responseJSON)
+        const phoneS = responseJSON.data;
+        console.log(phoneS)
+        for (let i = 0; i < phoneS.length; i++) {
+            console.log(phoneS[i].phones)
+            phoneContainer.innerHTML += `<li>${phoneS[i].phones}</li>`
+        }
+
+    }catch(error) {
+        phoneContainer.innerHTML += `<h1>there is an major error happening<h1>`
+        
+    }
+}
+detailContainer()
--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function phoneNames() {
         const phoneData = responseJSON.data;
         for (let i = 0; i < phoneData.length; i++) {
             console.log(phoneData[i].brand_name)
-            if (i === 50) {
+            if (i === 52) {
                 break
             }
             containerWithPhones.innerHTML += `<li class="phonenamesli"><a href="phonedetails.html?id=$">${phoneData[i].brand_name}</a></li>`
