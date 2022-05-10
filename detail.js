942bcf6d5b927c0351137d336f81d975ff7e4290
IsacHolstad <wisac22@gmail.com>
2022-05-10 20:07:00 +0200

    getting error

--- detail.js
+++ detail.js
@@ -1,9 +1,9 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const brand_id = params.get('brand_id')
+const id = params.get('id')
 
 
-const detailApi = `https://api-mobilespecs.azharimm.site/v2/brands/${brand_id}`;
+const detailApi = `https://api-mobilespecs.azharimm.site/v2/brands/${id}`;
 const proxy = "https://noroffcors.herokuapp.com/";
 const corsFixUrl = proxy + detailApi;
 
@@ -14,15 +14,15 @@ const phoneContainer = document.querySelector(".container");
 async function detailContainer() {
     try {
         const response = await fetch(corsFixUrl);
-        //console.log(response);
+        console.log(response);
         const phoneS = await response.json(); // convert the response to json dat
-        //console.log(phoneS)
+        console.log(phoneS)
             
             phoneContainer.innerHTML = `<li>${phoneS[i].brand_name}</li>`
         
 
     }catch(error) {
-        phoneContainer.innerHTML += `<h1>404<h1>`
+        phoneContainer.innerHTML = `<h1>404<h1>`
         
     }
 }
